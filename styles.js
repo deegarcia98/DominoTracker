b78fef37ab8c318d0354ca2caf56893f0731d1d4
Diosdado Garcia <deegarcia98@gmail.com>
2024-08-27 00:17:20 -0400

    added eventlistener to rules Button

--- styles.js
+++ styles.js
@@ -1,1 +1,15 @@
+const rulesOfGameBtn = document.getElementById("rulesofgame");
+
+rulesOfGameBtn.addEventListener("click", rulesMsg);
+
+function rulesMsg() {
+    alert("Once the winning team is determined (by placing last domino or lowest score), the losing team (and only the losing team) counts the value of their remaining fichas. This total is awarded to the winning team's score. The first team to reach 150 points wins the game.")
+};
+
+
+console.log(rulesOfGameBtn);
+
+
 const addRow = () => {};
+
+
