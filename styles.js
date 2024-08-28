14119c7708105c9b7aa96a307b0ab4829678b8b6
Diosdado Garcia <deegarcia98@gmail.com>
2024-08-28 00:12:02 -0400

    copied code from YT not sure if I will keep it

--- styles.js
+++ styles.js
@@ -7,9 +7,43 @@ function rulesMsg() {
 };
 
 
-console.log(rulesOfGameBtn);
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-modal-close]");
+const overlay = document.getElementById("overlay");
 
+console.log(openModalButtons);
 
-const addRow = () => {};
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if(modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if(modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
 
 
