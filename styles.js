f6ee22daa89933e2ddfb83783def8e230b492be5
Diosdado Garcia <deegarcia98@gmail.com>
2024-08-31 22:28:02 -0400

    worked on some code to get the value of what is in the input field for team names. still a work in progress

--- styles.js
+++ styles.js
@@ -10,4 +10,14 @@ closeModal.addEventListener('click', () => {
     modal.close();
 });
 
+const firstTeamName = document.getElementById('team1name').value;
 
+const secondTeamName = document.getElementById('team2name').value;
+
+let team1 = {firstTeamName};
+
+let team2 = {secondTeamName};
+
+window.localStorage.setItem('team1', 'team2');
+
+console.log(team1)
