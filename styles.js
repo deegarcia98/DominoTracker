3386c0b031efd3f097f44c263f6dc76283867849
Diosdado Garcia <deegarcia98@gmail.com>
2024-09-02 01:29:06 -0400

    starting the work on scoreboard

--- styles.js
+++ styles.js
@@ -17,26 +17,44 @@ const secondTeamName = document.getElementById('team2input');
 // window.localStorage.setItem('firstTeamName', 'secondTeamName');
 
 team1savebtn.addEventListener('click', () => {
-    let team1 = document.getElementById('team1input').value;
-    let nameDiv = document.getElementById('team1name');
+    const team1 = document.getElementById('team1input').value;
+    const nameDiv = document.getElementById('team1name');
     nameDiv.innerHTML = team1;
 
 })
 
 team2savebtn.addEventListener('click', () => {
-    let team2 = document.getElementById('team2input').value;
-    let nameDiv = document.getElementById('team2name');
+    const team2 = document.getElementById('team2input').value;
+    const nameDiv = document.getElementById('team2name');
     nameDiv.innerHTML = team2;
 
-    if(team2 === '') {
-        alert('Please enter in a Team Name')
-    }
 })
 
 const saveTeamName = () => {
 
 };
 
-function addScore() {};
+//<button id="add-score-btn1">Add Score</button>
+function addScoreTeam1() {
+    const team1score = document.getElementById('add-score-btn1');
+    const scoreTable = document.getElementById('table-score');
+    team1score.addEventListener('click', () => {
+        
+    })
+};
+
+const newScoreBoard
+
+const cell1 = team1;
+cosnt cell2 = team2
+
+const 
+
+
+
+function addScoreTeam2() {
+    
+};
+
 
 
