4d57243c70899fcbeea137b274bace48812a8141
Diosdado Garcia <deegarcia98@gmail.com>
2024-09-02 23:01:44 -0400

    cleared team name inputs when save button clicked

--- styles.js
+++ styles.js
@@ -20,27 +20,43 @@ team1savebtn.addEventListener('click', () => {
     const team1 = document.getElementById('team1input').value;
     const nameDiv = document.getElementById('team1name');
     nameDiv.innerHTML = team1;
-
+    clearTeam1();
 })
+
+function clearTeam1() {
+    firstTeamName.value = "";
+}
+
+
 
 team2savebtn.addEventListener('click', () => {
     const team2 = document.getElementById('team2input').value;
     const nameDiv = document.getElementById('team2name');
     nameDiv.innerHTML = team2;
-
+    clearTeam2();
 })
+
+function clearTeam2() {
+    secondTeamName.value = "";
+}
+
+
 
 const saveTeamName = () => {
 
 };
 
 //<button id="add-score-btn1">Add Score</button>
+const addScoreBtn1 = document.getElementById('add-score-btn1');
+
+addScoreBtn1.addEventListener('click', () => {
+        addScoreTeam1();
+})
+
 function addScoreTeam1() {
     const team1score = document.getElementById('add-score-btn1');
     const scoreTable = document.getElementById('table-score');
-    team1score.addEventListener('click', () => {
-        
-    })
+    2
 };
 
 //const newScoreBoard
