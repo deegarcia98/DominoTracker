371d2a6d37060c5e0405d1bbc9409a6942af5d6a
Diosdado Garcia <deegarcia98@gmail.com>
2024-09-05 22:26:06 -0400

    slowly refactoring function addScoreBtn2

--- styles.js
+++ styles.js
@@ -56,65 +56,70 @@ Think of how you can apply this to your code.
 for more ideas/help         https://www.youtube.com/watch?v=aAAS9cEuFYI
 */
 
-const team1 = {
-    name: 'firstTeamName',
-    score: 0,
+// const team1 = {
+//     name: 'firstTeamName',
+//     score: 0,
     
-};
+// };
 
-const team2 = {
-    name: 'secondTeamName',
-    score: 'team2score',
+// const team2 = {
+//     name: 'secondTeamName',
+//     score: 'team2score',
 
-};
-
-
+// };
 
 
-//          Code for Adding the Score       https://www.youtube.com/watch?v=zWIyJdJW6t8
 
-const team1Score = document.getElementById('score-input1');
-const team2Score = document.getElementById('score-input2');
+
+//          Code for Adding the Score       
+
+const scoreInput1 = document.getElementById('score-input1');
+const scoreInput2 = document.getElementById('score-input2');
 
 const addScoreBtn1 = document.getElementById('add-score-btn1');
 const addScoreBtn2 = document.getElementById('add-score-btn2');
 
+const roundScore1 = document.getElementById('round-score1');
+const roundScore2 = document.getElementById('round-score2');
+
+let scoreOfTeam1 = 0;
+let scoreOfTeam2 = 0;
+
+ roundScore1.innerHTML = scoreOfTeam1;
+ roundScore2.innerHTML = scoreOfTeam2;
+
 const tr = document.getElementById('scoreboard-tr')
 
+
 addScoreBtn1.addEventListener('click', () => {
-        addScoreTeam1();
+    addScoreTeam1();
 })
 
-
-const roundScore1 = document.getElementById('round-score1');
-
-
 function addScoreTeam1() {
-    let score1 = team1Score.value;
-    let score2 = '0'
+    let score1 = scoreInput1.value;
+    let score2 = scoreInput2.value;
 
     const template = `
     <td id='round-score1' class='score'>${score1}</td>
     <td id='round-score2' class='score'>${score2}</td>
 `
 tr.innerHTML = template;
-
 }
 
 addScoreBtn2.addEventListener('click', () => {
-    addScoreTeam2();
+    addScoreTeam2()
 })
 
 function addScoreTeam2() {
-    roundscore1 = team1Score;
-    let score2 = team2.score;
-
-    
+    roundScore2.innerHTML = scoreInput2.value;
 };
 
-console.log(addScoreTeam2)
 
-//cosnt newTd2 = document.createElement('td')
+/* I need to make it so that when the Team1Btn is pressed the score is added,
+but the score of team 2 is not changed or equals 0. 
+
+
+*/
 
 
 
