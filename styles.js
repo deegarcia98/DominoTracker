3881556a68e7ff4a8dab7937bc000995d10674ce
Diosdado Garcia <deegarcia98@gmail.com>
2024-11-20 12:42:24 -0500

    working on adding cells func

--- styles.js
+++ styles.js
@@ -52,22 +52,22 @@ team2 = {
 //          Code for setting the Team Name
 
 team1savebtn.addEventListener("click", () => {
-  addName(team1);
-  clearTeamName(team1);
+  addName(team1)
+  clearTeamName(team1)
 });
 
 team2savebtn.addEventListener("click", () => {
-  addName(team2);
-  clearTeamName(team2);
+  addName(team2)
+  clearTeamName(team2)
 });
 
 function addName(team) {
-  team.nameDiv.innerHTML = team.nameInput.value;
+  team.nameDiv.innerHTML = team.nameInput.value
 }
 
 function clearTeamName(team) {
-  team.nameInput.value = "";
-  //team.nameInput.value = "";
+  team.nameInput.value = ""
+  //team.nameInput.value = ""
 }
 
 // Saving team names and score when enter is pressed
@@ -75,7 +75,7 @@ team1.nameInput.addEventListener("keyup", (e) => {
   e.preventDefault();
   if (e.key === "Enter") {
     //console.log("Enter has been pressed");
-    team1savebtn.click();
+    team1savebtn.click()
   }
 });
 
@@ -83,23 +83,23 @@ team2.nameInput.addEventListener("keyup", (e) => {
   e.preventDefault();
   if (e.key === "Enter") {
     //console.log("Enter has been pressed");
-    team2savebtn.click();
+    team2savebtn.click()
   }
 });
 
 team1.scoreInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    e.preventDefault();
-    validateScore();
-    team1.scoreButton.click();
+    e.preventDefault()
+    validateScore()
+    team1.scoreButton.click()
   }
 });
 
 team2.scoreInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    e.preventDefault();
-    validateScore();
-    team2.scoreButton.click();
+    e.preventDefault()
+    validateScore()
+    team2.scoreButton.click()
   }
 });
 
@@ -112,7 +112,7 @@ team2.scoreInput.addEventListener("keypress", (e) => {
 // }
 
 function hasWon(score) {
-  return score >= 150;
+  return score >= 150
   // if(score >= 150) {
   //     return true
   // }
@@ -120,31 +120,31 @@ function hasWon(score) {
 }
 
 //          Code for Adding the Score
-team1.scoreDiv.innerHTML = team1.currentScore;
-team2.scoreDiv.innerHTML = team2.currentScore;
+team1.scoreDiv.innerHTML = team1.currentScore
+team2.scoreDiv.innerHTML = team2.currentScore
 
 team1.scoreButton.addEventListener("click", () => {
-  //loadElement(team1);
-  newHistorRow();
-  handleGame(team1);
-  clearScoreInput(team1);
-  showHistory(team1);
+  newHistorRow(team1)
+  handleGame(team1)
+  clearScoreInput(team1)
+  // showHistory(team1);
 });
 
 team2.scoreButton.addEventListener("click", () => {
-  handleGame(team2);
-  clearScoreInput(team2);
-  showHistory(team2);
+  newHistorRow(team2)
+  handleGame(team2)
+  clearScoreInput(team2)
+  //showHistory(team2);
 });
 
 // handles the score, innerHTML of total score and if wins congrats message
 function handleGame(team) {
-  team.currentScore += validateScore(team.scoreInput.value);
-  team.scoreDiv.innerHTML = team.currentScore;
+  team.currentScore += validateScore(team.scoreInput.value)
+  team.scoreDiv.innerHTML = team.currentScore
   storeScore(team);
-  let win = hasWon(team.currentScore);
+  let win = hasWon(team.currentScore)
   if (win) {
-    winnerAlert(team.nameDiv.innerHTML);
+    winnerAlert(team.nameDiv.innerHTML)
   }
 }
 
@@ -152,79 +152,83 @@ function handleGame(team) {
 
 function showHistory(team) {
   for (let i = 0; i < team.scoreHistory.length; i++) {
-    team.historyDiv.innerHTML = team.scoreHistory;
-    console.log(team1.scoreHistory[i]);
+    team.historyDiv.innerHTML = team.scoreHistory
+    console.log(team1.scoreHistory[i])
   }
 }
 
 function storeScore(team) {
-  team.scoreHistory.push(validateScore(team.scoreInput.value));
+  team.scoreHistory.push(validateScore(team.scoreInput.value))
 }
 
 function validateScore(score) {
   if (!score) {
     return 0;
   }
-  let scoreInt = parseInt(score);
+  let scoreInt = parseInt(score)
   if (0 < scoreInt < 150) {
     return scoreInt;
   }
 }
 
 function clearScoreInput(team) {
-  team.scoreInput.value = "";
+  team.scoreInput.value = ""
 }
 
 function winnerAlert(teamname) {
-  alert(`Congratulations ${teamname}, You are the Winner.`);
+  alert(`Congratulations ${teamname}, You are the Winner.`)
 }
 
 function editButton(teamscore) {}
 
-const resetButton = document.getElementById("reset-game-btn");
+const resetButton = document.getElementById("reset-game-btn")
 
 resetButton.addEventListener("click", () => {
-  resetGame(team1);
-  resetGame(team2);
+  resetGame(team1)
+  resetGame(team2)
 });
 
 function resetGame(team) {
-  team.nameDiv.innerHTML = "";
-  team.scoreDiv.innerHTML = 0;
-  team.scoreHistory = 0;
+  team.nameDiv.innerHTML = ""
+  team.scoreDiv.innerHTML = 0
+  team.scoreHistory = 0
 }
 
 function addScoreToTable() {
   // Get the table body element
-  const tableBody = document.getElementById("scoreTableBody");
+  const tableBody = document.getElementById("scoreTableBody")
   //Create new row
-  const newRow = tableBody.insertRow();
+  const newRow = tableBody.insertRow()
   // Create a new cell for the score
-  const scoreCell = newRow.insertCell();
+  const scoreCell = newRow.insertCell()
   // Add the score to the cell
-  scoreCell.textContent = score;
+  scoreCell.textContent = score
 }
 
 function loadElement() {
-  let main = document.getElementById("tableRow");
-  let tr = document.createElement("tr");
-  tr.textContent = "This paragraph was created by insertAdjacentElement";
-  return main.insertAdjacentElement("beforeEnd", tr);
+  let main = document.getElementById("tableRow")
+  let tr = document.createElement("tr")
+  tr.textContent = "This paragraph was created by insertAdjacentElement"
+  return main.insertAdjacentElement("beforeEnd", tr)
   // beforeBegin, afterBegin, beforeEnd, afteEnd
 }
 
 
 function newHistorRow() {
-  let main = document.getElementById("score-history-container");
-  let newRow = main.insertRow(-1);
-  let newText = document.createTextNode('REEEEEEE')
+  let main = document.getElementById("score-history-container")
+  let newRow = main.insertRow(-1)
+  let newText = document.createTextNode(`${team.scoreInput.value}`)
   newRow.appendChild(newText)
   //newRow.textContent = "This paragraph was created by insertAdjacentElement";
   // return main.insertAdjacentElement("beforeEnd", newRow);
   // beforeBegin, afterBegin, beforeEnd, afteEnd
 }
 
-
+// function is going to be creating a new row when either teams score button is pressed
+// we need to add a new table row with both cells
+// display the value of team.scoreInput in the corresponding side of team that won the round
+// then we make the other teams score 0
+// not sure if the edit button can be added seperately but if so add edit button
 
 
 
